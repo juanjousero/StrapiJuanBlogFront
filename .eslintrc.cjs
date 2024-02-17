a289afc0315345714ec9b92ab61854cd349c567e
Juanjo <usero481@gmail.com>
2024-02-17 09:14:34 +0100

    feat: Install prettier and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,38 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['prettier', 'airbnb'],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs, jsx}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
     ],
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-no-target-blank': 'off',
-    'react/button-has-type': 'off',
-    'react/jsx-one-expression-per-line': 'off',
-    'react/function-component-definition': 'off',
-    'react/prop-types': 'off',
-    'no-shadow': 'off',
-    'import/prefer-default-export': 'off',
-    'comma-dangle': 'off',
-  },
-}
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-no-target-blank': 'off',
+        'react/button-has-type': 'off',
+        'react/jsx-one-expression-per-line': 'off',
+        'react/function-component-definition': 'off',
+        'react/prop-types': 'off',
+        'no-shadow': 'off',
+        'import/prefer-default-export': 'off',
+        'comma-dangle': 'off',
+        'no-nested-ternary': 'off',
+        camelcase: 'warn',
+        'max-len': 'off',
+        'no-unsafe-optional-chaining': 'off',
+        'arrow-parens': 'off',
+    },
+};
