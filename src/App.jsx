aaa97289ccb66a62ffedcd2b7872251faeede631
Juanjo <usero481@gmail.com>
2024-02-17 09:15:46 +0100

    feat: Improve general styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Hero } from './components/Hero/Hero';
 import './App.css';
 
 export function App() {
-  return (
-    <Hero
-      title="Hero inicial de ejemplo"
-      description="Descripción de ejemplo del Hero"
-      backgroundImage="/src/assets/HomeHero.webp"
-      backgroundType="clear"
-      textPlacement="center"
-    />
-  );
+    return (
+        <main>
+            <Hero
+                title="Hero inicial de ejemplo"
+                description="Descripción de ejemplo del Hero que quiero hacer muy muy larga para comprobar que el screen__wrapper funciona bien, a ver qué sale"
+                backgroundImage="/src/assets/HomeHero.webp"
+                backgroundType="clear"
+                textPlacement="leftDown"
+            />
+        </main>
+    );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,37 +5,41 @@ import './Hero.css';
 const HeroFooter = () => <p>Footer from Hero</p>;
 
 export const Hero = ({
-  title,
-  description,
-  buttonText,
-  onClickButton,
-  heroFooter,
-  backgroundImage,
-  backgroundType,
-  textPlacement,
+    title,
+    description,
+    buttonText,
+    onClickButton,
+    heroFooter,
+    backgroundImage,
+    backgroundType,
+    textPlacement,
 }) => {
-  const possibleBackgroundTypes = {
-    clear: 'Background-clear',
-    dark: 'Background-dark',
-  };
+    const possibleBackgroundTypes = {
+        clear: 'Background-clear',
+        dark: 'Background-dark',
+    };
 
-  const possibleTextPlacements = {
-    leftDown: 'Hero-text-leftDown',
-    center: 'Hero-text-center',
-  };
+    const possibleTextPlacements = {
+        leftDown: 'Hero-text-leftDown',
+        center: 'Hero-text-center',
+    };
 
-  return (
-    <section
-      className={`Hero ${possibleBackgroundTypes[backgroundType]} ${possibleTextPlacements[textPlacement]}`}
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-        color: backgroundType,
-      }}
-    >
-      <h1>{title}</h1>
-      {description && <h2>{description}</h2>}
-      {buttonText && <Button onClick={onClickButton}>{buttonText}</Button>}
-      {heroFooter && <HeroFooter />}
-    </section>
-  );
+    return (
+        <section
+            className="Hero"
+            style={{
+                backgroundImage: `url(${backgroundImage})`,
+                color: backgroundType,
+            }}
+        >
+            <div
+                className={`Hero-container ${possibleBackgroundTypes[backgroundType]} ${possibleTextPlacements[textPlacement]} screen__wrapper`}
+            >
+                <h1>{title}</h1>
+                {description && <h2>{description}</h2>}
+                {buttonText && <Button onClick={onClickButton}>{buttonText}</Button>}
+                {heroFooter && <HeroFooter />}
+            </div>
+        </section>
+    );
 };
