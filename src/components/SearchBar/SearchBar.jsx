674b6cab9a184307deeb6e8e2d48af60bad3f8c5
Juanjo <usero481@gmail.com>
2024-02-17 10:17:11 +0100

    git commit -m "feat: Improve SearchBar"

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,6 +2,6 @@ import './SearchBar.css';
 
 export const SearchBar = ({ onSubmit }) => (
     <form className="SearchBar" onChange={onSubmit}>
-        <input maxLength={50} />
+        <input maxLength={50} placeholder="Viaje a NY..." />
     </form>
 );
