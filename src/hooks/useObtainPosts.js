287e45367a3f358b9c90a083768a2dc02ac5e478
Juanjo <usero481@gmail.com>
2024-02-18 16:54:45 +0100

    feat: Create api for posts and useObtainPosts hook

---
+++ src/hooks/useObtainPosts.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { getPostsEndpoint } from '../api/posts';
+
+export const useObtainPosts = () => {
+    const [posts, setPosts] = useState();
+
+    const storagePosts = ({ posts }) => {
+        return posts?.map(post => ({
+            id: post.ID,
+            author: post.author,
+            postDate: post.date,
+            title: post.title,
+            image: post.post_thumbnail,
+            tags: post.tags,
+            categories: post.categories,
+            content: post.content,
+        }));
+    };
+
+    useEffect(() => {
+        fetch(getPostsEndpoint)
+            .then(response => response.json())
+            .then(data => setPosts(storagePosts(data)));
+    }, []);
+
+    return { posts };
+};
