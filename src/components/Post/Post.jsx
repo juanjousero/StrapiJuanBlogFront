a70584cd9a073f086d532a47c5313d5222f1df4e
Juanjo <usero481@gmail.com>
2024-02-18 16:55:10 +0100

    feat: Include hook for WP posts on components

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,14 +1,15 @@
 import './Post.css';
 
 export const Post = ({ detail }) => {
-    const { img, title, description } = detail;
+    const { image, title, content: description } = detail;
 
     return (
         <article className="Post">
-            {img && <img src={img} />}
+            {image && <img src={image?.URL} />}
             <div className="Post-container">
                 <h3>{title}</h3>
                 <p className="Post-container__note">{description}</p>
+                {/* {description} */}
             </div>
         </article>
     );
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -3,13 +3,23 @@ import { Post } from '../Post/Post';
 import mockedData from '../../mocks/posts.json';
 
 import './Posts.css';
+import { useObtainPosts } from '../../hooks/useObtainPosts';
 
 export const Posts = () => {
     const [posts, setPosts] = useState();
+    const { posts: wordpressPosts } = useObtainPosts();
+
+    console.log({ wordpressPosts });
 
     useEffect(() => {
-        setPosts(mockedData.posts);
-    }, []);
+        if (wordpressPosts && wordpressPosts.length > 0) {
+            setPosts(wordpressPosts);
+        }
+    }, [wordpressPosts]);
+
+    // useEffect(() => {
+    //     setPosts(mockedData.posts);
+    // }, []);
 
     return (
         <section className="Posts screen__wrapper">
